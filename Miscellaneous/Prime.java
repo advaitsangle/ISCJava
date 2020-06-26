4d631a5869b92485a4d90e54a7757c50184e5273
Advait Sangle <66056468+advaitsangle@users.noreply.github.com>
2020-06-26 09:14:35 +0530

    Create Prime.java

---
+++ Miscellaneous/Prime.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+public class Prime{
+    public static void main(){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter number");
+        int num = sc.nextInt();
+        int factors = 0;
+        for(int k = 2; k<=num/2; k++)
+            if(num%k==0) factors++;
+        if(factors == 0)System.out.print("Prime Number");
+        else System.out.println("Not a Prime Number");   
+    }
+}
