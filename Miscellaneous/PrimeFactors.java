afb915dbfc5df9892030c76ed69b8e789a2e614b
Advait Sangle <66056468+advaitsangle@users.noreply.github.com>
2020-07-03 10:08:06 +0530

    Update PrimeFactors.java

--- Miscellaneous/PrimeFactors.java
+++ Miscellaneous/PrimeFactors.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 public class PrimeFactors{
-    public static void main(){
+    public static void main(String args[]){
         Scanner sc = new Scanner(System.in);
         PrimeFactors obj = new PrimeFactors();
         System.out.println("Enter number");
